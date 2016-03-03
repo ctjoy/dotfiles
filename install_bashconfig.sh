2c6c6126447965a1509967b947614f55fa5f77b5
ctjoy <ctjoychen@gmail.com>
2016-03-03 06:17:05 +0000

    add tmux config and add bashrc

--- install_bashconfig.sh
+++ install_bashconfig.sh
@@ -2,4 +2,6 @@
 cd ~/.dotfiles/bash
 cat ~/.dotfiles/bash/bashrc > ~/.bashrc
 cat ~/.dotfiles/bash/bash_profile > ~/.bash_profile
+cat ~/.dotfiles/bash/bash_aliases > ~/.bash_aliases
+cat ~/.dotfiles/bash/bash_logout > ~/.bash_logout
 echo "Installed bashconfig successfully :-)"
---
+++ install_tmuxconfig.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~/.dotfiles/tmux
+cat ~/.dotfiles/tmux/tmux.conf > ~/.tmux.conf
+echo "Installed tmux config successfully :-)"
