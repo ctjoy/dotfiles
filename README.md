b6dcffec392444560248fc654702afda11debfbf
ctjoy <ctjoychen@gmail.com>
2016-04-05 10:19:05 +0800

    test

--- README.md
+++ README.md
@@ -5,4 +5,3 @@ You can see more detail in [ My Dotfiles Notebook ](https://ctjoy.gitbooks.io/my
 ## Installation
 
     git clone https://github.com/ctjoy/dotfiles.git ~/.dotfiles
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
--- install_vimconfig.sh
+++ install_vimconfig.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd ~/.dotfiles/vim
 cat ~/.dotfiles/vim/vimrc > ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cp -r ~/.dotfiles/vim/skeleton ~/.vim
 echo "Installed vimrc and vim plugin successfully :-)"
