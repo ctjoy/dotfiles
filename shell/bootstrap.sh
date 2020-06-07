b3dd7f104e54cb5d3801973de1e70e0ae4c083b8
Joy Chen <ctjoychen@gmail.com>
2020-06-07 12:50:38 +0800

    add diff-so-fancy

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -1,6 +1,9 @@
 path_prepend "$HOME/.local/bin"
 path_prepend "$HOME/.dotfiles/bin"
 
+# diff-so-fancy
+path_prepend "$HOME/.dotfiles/shell/plugins/diff-so-fancy/"
+
 # Base16 Shell
 BASE16_SHELL="$HOME/.dotfiles/shell/plugins/base16-shell/"
 [ -n "$PS1" ] && \
